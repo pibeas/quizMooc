a3dcfb55b882faa98a21f69845aec39bf5c08d10
pibeas <pedroitrabajo@gmail.com>
2015-08-20 17:43:35 +0200

    inicializar bd

--- models/models.js
+++ models/models.js
@@ -53,24 +53,14 @@ exports.User = User;
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
-  User.count().then(function (count){
-    if(count === 0) {   // la tabla se inicializa solo si está vacía
+ // la tabla se inicializa solo si está vacía
       User.bulkCreate( 
         [ {username: 'admin',   password: '1234', isAdmin: true},
           {username: 'pepe',   password: '5678'} // el valor por defecto de isAdmin es 'false'
-        ]
-      ).then(function(){
-        console.log('Base de datos (tabla user) inicializada');
-        Quiz.count().then(function (count){
-          if(count === 0) {   // la tabla se inicializa solo si está vacía
+        ]);
+    // la tabla se inicializa solo si está vacía
             Quiz.bulkCreate( 
               [ {pregunta: 'Capital de Italia',   respuesta: 'Roma', UserId: 2}, // estos quizes pertenecen al usuario pepe (2)
                 {pregunta: 'Capital de Portugal', respuesta: 'Lisboa', UserId: 2}
-              ]
-            ).then(function(){console.log('Base de datos (tabla quiz) inicializada')});
-          };
-        });
-      });
-    };
-  });
+              ]);
 });
